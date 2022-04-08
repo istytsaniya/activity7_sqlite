c6ff69c5dd9ce5a39293764ae49e73f162409937
Istytsaniya <tsaniyaisty08@gmail.com>
2022-04-08 11:15:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/activity6_sqlite/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.activity6_sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.activity6_sqlite.adapter.TemanAdapter;
+import com.example.activity6_sqlite.database.DBController;
+import com.example.activity6_sqlite.database.Teman;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private TemanAdapter adapter;
+    private ArrayList<Teman> temanArrayList;
+    DBController controller = new DBController(this);
+    String id,nm,tlp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        //BacaData();
+        adapter = new TemanAdapter(temanArrayList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+    public void BacaData(){
+        ArrayList<HashMap<String,String>> daftarTeman = controller.getAllTeman();
+    }
+}
